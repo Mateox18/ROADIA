194395755f7245b4ac2e8eaabcf976e0d9849a73
MateoP <Mateopardox18@gmail.com>
2025-04-04 09:58:31 -0500

    Terminal cambios

--- ROADIA.py
+++ ROADIA.py
@@ -1,27 +1,19 @@
-# -*- coding: utf-8 -*-
+
 """
 MODELO ROADIA BASADO EN LLAMA3
 VERSION ALPHA
 """
-import chromadb
 import os
 import sys
 import ollama
-from sentence_transformers import SentenceTransformer
-from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction 
-# Inicializa el modelo, chromadb y la collection con la información
-modelo = SentenceTransformer("all-MiniLM-L6-v2")
-embedding_function = SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+
 # Cuando se convierta en .exe va a ser capaz de leer los archivos de chroma y json
 def ruta_absoluta(relativa):
     if getattr(sys, 'frozen', False):
         return os.path.join(sys._MEIPASS, relativa)
     return os.path.join(os.path.dirname(__file__), relativa)
 
-# Conectar con ChromaDB
-ruta_chroma = ruta_absoluta("chroma_db")
-chroma_client = chromadb.PersistentClient(path=ruta_chroma)
-collection = chroma_client.get_or_create_collection("rutas_universidad", embedding_function=embedding_function)
+
 print("Version: Alpha, Unicamente funciona para rutas directas en este momento.")
 def extraer_info(texto):
     #Usa llama3 para extraer el origen y destino de la ruta
@@ -47,28 +39,11 @@ def extraer_info(texto):
     return eval(respuesta["message"]["content"])
 
 def buscar_ruta(origen, destino):
-    #Busca la mejor ruta en chromadb
-    #Primero se prueba una busqueda exacta
-    docs = collection.get(include=["documents"])
-    for ruta in docs["documents"]:
-        if origen in ruta and destino in ruta:
-            return ruta 
-    #Si no se cumple se prueba busqueda semantica
-    consulta = f"""
-    Encuentra una ruta accesible desde {origen} hasta {destino} en la Universidad de los Andes." """
-    query_embedding = modelo.encode([consulta]).tolist()
-    resultados = collection.query(query_embeddings=query_embedding, n_results=5)
-
-    query_embedding = modelo.encode([consulta]).tolist()
-    #Busca hasta 5 rutas que lleven al destino
-    resultados = collection.query(query_embeddings=query_embedding, n_results=3)
-    print(f"🔎 Resultados de ChromaDB para {origen} → {destino}: {resultados['documents']}")
-    if resultados["documents"] and resultados["documents"][0]:
-        return resultados["documents"][0][0]  # Retorna la mejor coincidencia
-    return "No se encontró una ruta accesible entre ambos lugares."
+    pass
+#Yo hago esto
 
 def chatbot():
-
+#Consola, terminal interfaz etc revisa esto.
     print("\n🚀 Hola soy ROADIA tu amigo en el camino, ¿A donde vamos hoy? (escribe 'salir' para terminar) \n")
     while True:
         consulta = input("🗺️ Pregunta sobre rutas: ")
